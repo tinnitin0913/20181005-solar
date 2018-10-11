99b86dddf333173555a6d1719268e8e778f28082
Charlotte Tseng <105405069@mail2.nccu.tw>
2018-10-12 02:49:28 +0800

    homework_20181005

---
+++ Assets/sun_2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun_2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        Vector3 angle = new Vector3(0, -0.8f, 0);
+        transform.Rotate(angle);
+    }
+}
